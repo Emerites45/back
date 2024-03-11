7e367b5bd071e4977bb4d26b45a9256efd7b47b1
emerites45 <emerites45@gmail>
2024-03-11 19:24:28 +0100

    Committing local changes

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ sequelize.sync().then( ()=>console.log('base de donnée pret'));
 
 const options = {
   key:fs.readFileSync('./certificates/key.pem'),
-  cert: fs.readFileSync('./certificates/cert.pem')
+  cert: fs.readFileSync('./certificates/cert.pem'),
+  csr:fs.readFileSync('./certificates/csr.pem')
 }
 
 // http://193.203.190.101:3000
